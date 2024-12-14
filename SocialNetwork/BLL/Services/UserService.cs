5e34a8866e578162d0c08d3de0179134b63af3bb
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-12-14 16:50:13 +0700

    1

---
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using SocialNetwork.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        public void Register(UserRegistrationData userRegistrationData)
+        {
+            if (string.IsNullOrEmpty(userRegistrationData.FirstName))
+                throw new ArgumentNullException(); 
+
+            if (string.IsNullOrEmpty(userRegistrationData.LastName))
+                throw new ArgumentNullException();
+
+            if (string.IsNullOrEmpty(userRegistrationData.Password))
+                throw new ArgumentNullException();
+
+            if (string.IsNullOrEmpty(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRegistrationData.Password.Length < 8)
+                throw new ArgumentOutOfRangeException();
+
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
