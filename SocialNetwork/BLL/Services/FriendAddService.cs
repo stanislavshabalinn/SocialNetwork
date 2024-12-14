50f0900d88fd2b806929f11ddb3a13b231864edd
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-12-14 17:53:19 +0700

    2

---
+++ SocialNetwork/BLL/Services/FriendAddService.cs
@@ -0,0 +1,46 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class FriendAddService
+    {
+        IFriendRepository friendRepository;
+        IUserRepository userRepository;
+
+        public FriendAddService()
+        {
+            friendRepository = new FriendRepository();
+            userRepository = new UserRepository();
+        }
+
+        public IEnumerable<User> GetFriendsUserById(int userid)
+        {
+            var userService = new UserService();
+            return friendRepository.FindAllByUserId(userid)
+                          .Select(friendsEntity => userService.FindById(friendsEntity.friend_id));
+        }
+
+        public void AddFriend(AddFriendsData addingFriendData)
+        {
+            var findUserEntity = this.userRepository.FindByEmail(addingFriendData.FriendEmail);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            var friendEntity = new FriendEntity()
+            {
+                user_id = addingFriendData.UserId,
+                friend_id = findUserEntity.id
+            };
+
+            if (this.friendRepository.Create(friendEntity) == 0)
+                throw new Exception();
+
+        }
+    }
+}
