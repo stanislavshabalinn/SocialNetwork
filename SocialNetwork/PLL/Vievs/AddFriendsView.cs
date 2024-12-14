a8113d1ec7e41d77de61f6bc84669270b412a4ee
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-12-14 18:18:50 +0700

    3

---
+++ SocialNetwork/PLL/Vievs/AddFriendsView.cs
@@ -0,0 +1,49 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class AddFriendsView
+    {
+        FriendAddService friendService;
+
+        public AddFriendsView(FriendAddService friendService)
+        {
+            this.friendService = friendService;
+        }
+
+        public void Show(User user)
+        {
+            var addFriensdData = new AddFriendsData();
+
+            Console.Write("Введите почтовый адрес друга: ");
+            addFriensdData.FriendEmail = Console.ReadLine();
+            addFriensdData.UserId = user.Id;
+
+            try
+            {
+                friendService.AddFriend(addFriensdData);
+                AddFriendMessage.Show("Друг успешно добавлен!");
+
+            }
+
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Друг не найден!");
+            }
+
+            catch (Exception)
+            {
+                AlertMessage.Show("Ошибка при добавлении в друзья!");
+            }
+
+        }
+
+    }
+}
---
+++ SocialNetwork/PLL/Vievs/UserFriendsView.cs
@@ -0,0 +1,28 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserFriendsView
+    {
+        public void Show(IEnumerable<User> friends)
+        {
+            Console.WriteLine("Друзья:");
+
+            if (friends.Count() == 0)
+            {
+                Console.WriteLine("Друзей нет");
+                return;
+            }
+
+            friends.ToList().ForEach(friend =>
+            {
+                Console.WriteLine(friend.FirstName, friend.LastName, friend.Email);
+            });
+        }
+    }
+}
