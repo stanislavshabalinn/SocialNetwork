fd7d1589013282eb988e40f2252f37b38631dc78
shabalin_sa1@mail.ru <shabalin_sa1@mail.ru>
2024-12-17 15:44:44 +0700

    u

--- SocialNetwork/DAL/Repositories/BaseRepository.cs
+++ SocialNetwork/DAL/Repositories/BaseRepository.cs
@@ -37,7 +37,7 @@ namespace SocialNetwork.DAL.Repositories
 
         private IDbConnection CreateConnection()
         {
-            return new SQLiteConnection("Data Source = DAL/DB/social_network_bd.db; Version = 3");
+            return new SQLiteConnection("Data Source = C:\\Users\\shabalin_sa\\source\\repos\\SocialNetwork\\SocialNetwork\\SocialNetwork\\DAL\\DB\\social_network_bd.db; Version = 3");
         }
     }
 }
